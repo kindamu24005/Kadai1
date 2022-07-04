41d23fd342e096dee941e593122d4e2a13269180
kindamu24005 <107189394+kindamu24005@users.noreply.github.com>
2022-07-04 23:01:08 +0900

    in the seisuronfile

--- AOJ/kumiawase/1000000007.py
+++
@@ -1,7 +0,0 @@
-m,n = map(int,input().split())
-print(pow(m,n,1000000007)) #mのn乗を1000000007（1e+9より大きい最小の素数らしい）で割ったあまり
-
-#こっちだとタイムオーバー
-m,n = map(int,input().split())
-ans = int(m**n % 1000000007)
-print(ans)
